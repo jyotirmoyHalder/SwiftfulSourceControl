a7343e9a3eebac27a558e26851fe8aa319ffd192
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-01 17:30:06 +0600

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by jyotirmoy_halder on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
