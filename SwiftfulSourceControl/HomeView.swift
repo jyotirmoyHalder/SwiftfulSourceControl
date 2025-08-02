b31965b42e7f10ca52d1ef7e117f2ce54aa72fdf
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-02 10:38:36 +0600

    Add home view

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by jyotirmoy_halder on 2/8/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
