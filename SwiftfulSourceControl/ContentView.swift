e87632dfa4e9bd929bd98cb15464ee5c0c6accab
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-02 17:10:04 +0600

    [Feature] Text swiftful text to only switfful

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking")
+            Text("Swiftful!")
             Text("Two shape added")
             Button("Click Me") {
                 
