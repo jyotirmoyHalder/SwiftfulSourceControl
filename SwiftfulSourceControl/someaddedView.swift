da7e105d68d833251062aa6c6faf500a5145cd91
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-05 22:38:14 +0600

    [feature] Rectanle added to someaddedView

---
+++ SwiftfulSourceControl/someaddedView.swift
@@ -0,0 +1,19 @@
+//
+//  someaddedView.swift
+//  SwiftfulSourceControl
+//
+//  Created by jyotirmoy_halder on 5/8/25.
+//
+
+import SwiftUI
+
+struct someaddedView: View {
+    var body: some View {
+        Rectangle().fill(Color.red)
+            .frame(width: 300, height: 300)
+    }
+}
+
+#Preview {
+    someaddedView()
+}
