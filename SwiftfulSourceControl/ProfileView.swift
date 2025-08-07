7083be981f2836216bdf6f5837df3f5ad106c187
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-07 17:33:22 +0600

    [Feature] Add profile view

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by jyotirmoy_halder on 7/8/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
